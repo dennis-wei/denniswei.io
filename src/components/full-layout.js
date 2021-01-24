cee4ccb980762c5a4527c013767f9f757dc45c7d
Dennis Wei <dw2654@columbia.edu>
2021-01-24 00:51:19 -0500

    initial mood board

--- src/components/full-layout.js
+++ src/components/full-layout.js
@@ -10,11 +10,10 @@ import PropTypes from "prop-types"
 
 import "./layout.css"
 
-const Layout = ({ children }) => (
+const FullLayout = ({ children }) => (
   <div
     style={{
       margin: `0 8% 0 8%`,
-      maxWidth: 960,
       padding: `0px 1.0875rem 1.45rem`,
       paddingTop: `5%`,
     }}
@@ -23,8 +22,8 @@ const Layout = ({ children }) => (
   </div>
 )
 
-Layout.propTypes = {
+FullLayout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default FullLayout
---
+++ src/pages/2021-board.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import LayoutFull from "../components/full-layout"
+import SEO from "../components/seo"
+import Img from "gatsby-image"
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+function BoardPage2021({data}) {
+  return (
+    <LayoutFull>
+      <SEO title="2021 mood" keywords={[`2021`, `moodboard`]} />
+      <h1>2021 moods</h1>
+      <Carousel
+        infiniteLoop={true}
+        showStatus={false}
+        showIndicators={true}
+        autoPlay={false}
+        showArrows={false}
+        selectedItem={0}
+      >
+        <div style={{ maxHeight: "100%" }}>
+          <Img
+            style={{ maxHeight: "600px" }}
+            imgStyle={{ objectFit: "contain" }}
+            fluid={data.jan.childImageSharp.fluid}
+          />
+        </div>
+      </Carousel>
+    </LayoutFull>
+  )
+}
+
+export const query = graphql`
+  query {
+    jan: file(relativePath: { eq: "jan-2021.png" }) {
+      childImageSharp {
+        fluid(maxHeight: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+export default BoardPage2021
